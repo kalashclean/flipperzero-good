f67701f09621b71e08b75c08db7212548a6a17cd
kalashclean <63450519+kalashclean@users.noreply.github.com>
2023-12-21 14:05:52 +0100

    Update spi_mem_tools.h

--- spi_mem_manager/lib/spi/spi_mem_tools.h
+++ spi_mem_manager/lib/spi/spi_mem_tools.h
@@ -6,7 +6,7 @@
 #define SPI_MEM_MAX_BLOCK_SIZE 256
 #define SPI_MEM_FILE_BUFFER_SIZE 4096
 
-bool spi_mem_tools_read_chip_info(SPIMemChip* chip);
+//bool spi_mem_tools_read_chip_info(SPIMemChip* chip);
 bool spi_mem_tools_read_block(SPIMemChip* chip, size_t offset, uint8_t* data, size_t block_size);
 size_t spi_mem_tools_get_file_max_block_size(SPIMemChip* chip);
 SPIMemChipStatus spi_mem_tools_get_chip_status(SPIMemChip* chip);
