f4556421322087093661deb89e723ad373b8568b
kalashclean <63450519+kalashclean@users.noreply.github.com>
2023-12-21 10:58:20 +0100

    Update spi_mem_view_detect.c

--- spi_mem_manager/views/spi_mem_view_detect.c
+++ spi_mem_manager/views/spi_mem_view_detect.c
@@ -4,13 +4,13 @@
 
 struct SPIMemDetectView {
     View* view;
-    //IconAnimation* icon;
+    IconAnimation* icon;
     SPIMemDetectViewCallback callback;
     void* cb_ctx;
 };
 
 typedef struct {
-    //IconAnimation* icon;
+    IconAnimation* icon;
 } SPIMemDetectViewModel;
 
 View* spi_mem_view_detect_get_view(SPIMemDetectView* app) {
@@ -21,7 +21,7 @@ static void spi_mem_view_detect_draw_callback(Canvas* canvas, void* context) {
     SPIMemDetectViewModel* model = context;
     UNUSED(model);
     canvas_set_font(canvas, FontPrimary);
-    //canvas_draw_icon_animation(canvas, 0, 0, model->icon);
+    canvas_draw_icon_animation(canvas, 0, 0, model->icon);
     canvas_draw_str_aligned(canvas, 64, 26, AlignLeft, AlignCenter, "Detect");
     canvas_draw_str_aligned(canvas, 64, 36, AlignLeft, AlignCenter, "SPI chip...");
 }
@@ -29,15 +29,15 @@ static void spi_mem_view_detect_draw_callback(Canvas* canvas, void* context) {
 static void spi_mem_view_detect_enter_callback(void* context) {
     SPIMemDetectView* app = context;
     UNUSED(app);
-  //  with_view_model(
-  //      app->view, SPIMemDetectViewModel * model, { icon_animation_start(model->icon); }, false);
+    with_view_model(
+        app->view, SPIMemDetectViewModel * model, { icon_animation_start(model->icon); }, false);
 }
 
 static void spi_mem_view_detect_exit_callback(void* context) {
     SPIMemDetectView* app = context;
     UNUSED(app);
-    //with_view_model(
-    //    app->view, SPIMemDetectViewModel * model, { icon_animation_stop(model->icon); }, false);
+    with_view_model(
+        app->view, SPIMemDetectViewModel * model, { icon_animation_stop(model->icon); }, false);
 }
 
 SPIMemDetectView* spi_mem_view_detect_alloc() {
@@ -45,14 +45,14 @@ SPIMemDetectView* spi_mem_view_detect_alloc() {
     app->view = view_alloc();
     view_set_context(app->view, app);
     view_allocate_model(app->view, ViewModelTypeLocking, sizeof(SPIMemDetectViewModel));
-    /*with_view_model(
+    with_view_model(
         app->view,
         SPIMemDetectViewModel * model,
         {
             model->icon = icon_animation_alloc(&A_ChipLooking_64x64);
             view_tie_icon_animation(app->view, model->icon);
         },
-        false);*/
+        false);
     view_set_draw_callback(app->view, spi_mem_view_detect_draw_callback);
     view_set_enter_callback(app->view, spi_mem_view_detect_enter_callback);
     view_set_exit_callback(app->view, spi_mem_view_detect_exit_callback);
@@ -60,8 +60,8 @@ SPIMemDetectView* spi_mem_view_detect_alloc() {
 }
 
 void spi_mem_view_detect_free(SPIMemDetectView* app) {
-    //with_view_model(
-    //    app->view, SPIMemDetectViewModel * model, { icon_animation_free(model->icon); }, false);
+    with_view_model(
+        app->view, SPIMemDetectViewModel * model, { icon_animation_free(model->icon); }, false);
     view_free(app->view);
     free(app);
 }
